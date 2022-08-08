7696206ce05278fabd5bfbf5b2d36b80533121df
jyr <jyr@wavers.co.kr>
2022-08-08 09:45:42 +0900

    Initial commit

---
+++ app/src/main/java/io/jyryuitpro/app_architecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.jyryuitpro.app_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
